dc8ad1aaf775afdd35101d17b2643948e1aa8870
everson <everson@everson-PC>
2017-05-03 10:09:58 -0300

    Adicionado novos recursos

--- view/template.php
+++ view/template.php
@@ -1,16 +1,27 @@
 <html>
     <head>
-        <title>
-            
-        </title>
-        <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/style.css"/>
+        <title>Chat</title>
+        <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/template.css"/>
+         <script type="text/javascript" src="assets/js/jquery.min.js/>
+         <script type="text/javascript" src="assets/js/funcoes.js"/>
     </head>
     <body>
-        <h1>Topo do meu site</h1>
+        <div class="environment" style="background-color:<?php   
+            if(isset($_SESSION['area']) && $_SESSION['area'] == 'suporte'){
+                echo '#00ffff';
+            }elseif (isset($_SESSION['area']) && ($_SESSION['area']) == 'cliente'){
+                echo '#00ff00';
+            }  else {
+                echo '#000000';
+            }
+        ?>">
+            
+        </div>
+        <div class="container">
+            <?php $this->loadViewInTemplate($viewName, $viewData); ?>
+        </div>
+       
         
-        <?php $this->loadViewInTemplate($viewName, $viewData); ?>
-        
-        <h2>rodape do meu site</h2>
     </body>
 </html>
     
